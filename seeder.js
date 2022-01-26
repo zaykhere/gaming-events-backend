d67f0682dc8f283eea4cee056df126716f1ae6f7
Zain Javed <zainjhere@gmail.com>
2022-01-26 22:06:37 +0500

    Updating seeder.js

--- seeder.js
+++ seeder.js
@@ -2,41 +2,59 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-async function main() {
-  // await prisma.user.create({
-  //     data: {
-  //       name: 'Zain',
-  //       email: 'zain@gaming-events.com',
-  //       password: '123456',
-  //       isVerified: true
-  //     },
-  //   })
-
-//   await prisma.event.create({
-//     data: {
-//       name: "CSGO Tournament",
-//       slug: "csgo-tournament",
-//       venue: "Horizon Club",
-//       address: "919 3rd Ave New York, New York(NY), 10022",
-//       date: new Date("2022-06-09"),
-//       time: "10:00 PM EST",
-//       description: "CSGO Tournament featuring 2 5-a side teams",
-//       image: "/images/events-1.jpg",
-//       userId: 2
-//     },
-//   });
+async function createUser() {
+  await prisma.user.create({
+    data: {
+      name: "Hamza",
+      email: "hamza@gaming-events.com",
+      password: "123456",
+      isVerified: true,
+    },
+  });
 
   const allUsers = await prisma.user.findMany();
   console.dir(allUsers, { depth: null });
+}
+
+async function createEvent() {
+  await prisma.event.create({
+    data: {
+      name: "Fortnite Tournament",
+      slug: "fortnite-tournament",
+      venue: "Jackson Club",
+      address: "902 3rd Ave Pasadena, California(CA), 10022",
+      date: new Date("2022-03-09"),
+      time: "08:00 PM EST",
+      description: "Fortnite Tournament featuring 2 5-a side teams",
+      image: "/images/events-3.jpg",
+      userId: 3,
+    },
+  });
 
   const allEvents = await prisma.event.findMany();
   console.dir(allEvents, { depth: null });
 }
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+if (process.argv[2] === "-cu") {
+  createUser()
+    .catch((e) => {
+      throw e;
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+} else if (process.argv[2] === "-ce") {
+    createEvent()
+        .catch((e)=> {
+            throw e;
+        })
+        .finally(async () => {
+            await prisma.$disconnect();
+        })
+}
+else {
+  console.log("Command not recognized");
+  process.exit();
+}
+
+
